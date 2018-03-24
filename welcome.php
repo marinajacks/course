cade3282902728dd5af0eb41cf134bf3499d1aae
marinajacks <chenbiaozainan@163.com>
2018-03-24 16:46:48 +0800

    修改验证程序“ “ “ “ “

--- welcome.php
+++ welcome.php
@@ -1,16 +1,12 @@
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <body>
-
-欢迎 <?php echo $_POST["ip"]; ?><br>
-你的名字是: <?php echo $_POST["name"]; ?><br>
-你的密码是: <?php echo $_POST["password"]; ?><br>
-你的数据库是: <?php echo $_POST["database"]; ?><br>
+<?php
 
 
-	$servername = $_POST["ip"]
+	$servername = $_POST["ip"];
 	$username = $_POST["name"];
-	$password = $_POST["database"];
+	$password = $_POST["password"];
 	 
 	// 创建连接
 	$conn = new mysqli($servername, $username, $password);
@@ -20,5 +16,6 @@
 	    die("连接失败: " . $conn->connect_error);
 	} 
 	echo "连接成功";
+	?>
 </body>
 </html>
